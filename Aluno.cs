16001de0393bcc4c4f544f24d999980f7551743e
Guilherme Sousa Oliveira <guilhermesousa1110@gmail.com>
2023-04-24 11:03:37 -0300

    Inicia o projeto proposto em sala de aula

---
+++ Aluno.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cadastro_aluno
+{
+    public class Aluno
+    {
+        public string nome;
+        public string curso;
+
+        public string idade;
+        public string rg;
+        public bool bolsista;
+        public float mediaFinal;
+        public float valorMensalidade;
+
+        public void VerMediaFinal(){
+
+            Console.WriteLine($"Média final: {mediaFinal}");
+        }
+
+        public float VerMensalidade()
+        {
+            if(bolsista == true && mediaFinal >= 8){
+
+                return valorMensalidade / 2;
+            }
+
+            else if(bolsista == true && mediaFinal > 6)
+            {
+                return valorMensalidade * 0.7f;
+            }
+            
+            else{
+                return valorMensalidade;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// Cadastrar os dados de um aluno.
+
+// Criar uma classe Aluno, com as seguintes propriedades: Nome, Curso, Idade, RG, Bolsista(Bool), Média Final, Valor da mensalidade.
+
+// bolsista e média final maior ou igual a 8 conceder 50% de desconto na mensalidade
+// bolsista e média final maior que 6 e menor que 8 conceder 30% de desconto na mensalidade 
+// outros casos a mensalidade será integral
+
+// Receba os dados do cadastro via console e crie um menu para o usuário escolher se quer visualizar a média ou o valor da mensalidade.
+using cadastro_aluno;
+
+Aluno aluno1 = new Aluno();
+string escolhaBolsista;
+
+Console.WriteLine(@$"
+
+     Bem vindo ao sistema de cadastro de alunos!
+        
+     Digite o seu nome:
+");
+aluno1.nome = Console.ReadLine();
+
+Console.WriteLine($"Digite o curso que gostaria de cursar: ");
+aluno1.curso = Console.ReadLine();
+
+Console.WriteLine($"Digite a sua idade: ");
+aluno1.idade = Console.ReadLine();
+
+Console.WriteLine($"Digite o seu RG: ");
+aluno1.rg = Console.ReadLine();
+
+Console.WriteLine($"Você é bolsista? Digite '1' para sim ou '2' para não.");
+escolhaBolsista = Console.ReadLine();
+
+if()
+
+
+
